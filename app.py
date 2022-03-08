2882a3b73ff96ba2676e1c38b226dfbe33ed5b58
PassionOfStudy <35111721+PassionOfStudy@users.noreply.github.com>
2022-03-08 11:11:25 +0900

    Implement Main Page Frame

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import requests
 
 app = Flask(__name__)
 
-client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
-db = client.dbsparta_plus_week2
+# AWS 파이몽고 이용
+# client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+# db = client.dbsparta_plus_week2
+client = MongoClient('mongodb+srv://test:sparta@cluster0.jhcn4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.dbdracurrer
 
 
 @app.route('/')
