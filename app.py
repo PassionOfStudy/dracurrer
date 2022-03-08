14b185a0f13a20ade953fcf068c9aad8fc2ce290
PassionOfStudy <35111721+PassionOfStudy@users.noreply.github.com>
2022-03-09 08:41:39 +0900

    Implement register function

--- app.py
+++ app.py
@@ -6,18 +6,21 @@ import requests
 
 app = Flask(__name__)
 
+# 아틀라스 몽고 DB접속
 client = MongoClient('mongodb+srv://test:sparta@cluster0.jhcn4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = client.dbprojects
 
-@app.route('/', methods = ['POST', 'GET'])
+# 메인 
+@app.route('/')
 def main():
     return render_template('index.html')
     
-    
+# 게시글 등록페이지
 @app.route('/register')
 def registerPage():
     return render_template("register.html")
 
+# 등록페이지에서 사용자가 등록한 드라마 URL을 수신하여 필요한 메타정보를 추출하여 저장하는 API
 @app.route('/api/drama_lists', methods = ['POST'])
 def saveDramaList():
     # 등록페이지에서 Form태그에서 POST로 보낸 값 가져오기
@@ -34,7 +37,8 @@ def saveDramaList():
         html = data.content.decode('utf-8', 'replace')
         # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
         soup = BeautifulSoup(html, 'html.parser')
-
+        # 각 드라마 페이지의 메타정보를 추출
+        # Q) 메타태그인 og를 이용한이유? og태그의 경우 표준규약이기 때문에 웹페이지를 만들 떄 공통됨
         drama_title = soup.select_one('meta[property="og:title"]')['content'];
         drama_image = soup.select_one('meta[property="og:image"]')['content'];
         drama_desc = soup.select_one('meta[property="og:description"]')['content'];
@@ -47,6 +51,7 @@ def saveDramaList():
         db.dracurrer.insert_one(doc);
         return jsonify({'msg': '등록이 완료되었습니다!'})
 
+# MongoDB에 저장된 드라마관련 정보를 불러오는 API
 @app.route('/api/drama_lists', methods = ['GET'])
 def viewDramaList():
     drama_lists = list(db.dracurrer.find({}, {'_id':False}))
